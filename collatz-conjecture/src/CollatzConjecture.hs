be972f4b8d9f4b31ec69d6370c69da6e0ddba295
Chris NP <invigo.devel@gmail.com>
2018-05-03 01:50:03 +0300

    mod collatz conjecture

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -1,16 +1,15 @@
 module CollatzConjecture (collatz) where
 
-divBy2 :: Integer -> Integer
-divBy2 num = num `div` 2
-
-times3plus1 :: Integer -> Integer 
-times3plus1 num = ( 3 * num ) + 1
 
 collatzSteps :: Integer -> [Integer]
 collatzSteps 1 = [1]
 collatzSteps num 
     | even num  = num : collatzSteps ( divBy2 num ) 
     | otherwise = num : collatzSteps ( times3plus1 num )
+    where
+        divBy2 n = n `div` 2
+        times3plus1 n = ( 3 * n ) + 1
+
 
 collatz :: Integer -> Maybe Integer
 collatz 1 = Just 0
