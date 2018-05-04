8f447a50846751a422d65474605d35def384d5e6
Invigo <invigo.devel@gmail.com>
2018-05-04 23:16:08 +0300

    added sum-of-multiples

---
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -0,0 +1,17 @@
+module SumOfMultiples (sumOfMultiples) where
+
+import Data.List(nub)
+
+
+sumOfMultiples :: [Integer] -> Integer -> Integer
+sumOfMultiples [] limit      = 0
+sumOfMultiples factors limit = sum $ distinctFactors factors limit
+
+distinctFactors :: [Integer] -> Integer -> [Integer]
+distinctFactors [] limit     = []
+distinctFactors (x:xs) limit = nub $ (distinctFactors xs limit) ++ (appendFactor x limit 1)  
+
+appendFactor :: Integer -> Integer -> Integer -> [Integer]
+appendFactor factor limit index 
+    | factor * index >= limit = []
+    | otherwise               = (factor * index) : appendFactor factor limit (index + 1)
