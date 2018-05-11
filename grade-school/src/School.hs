55188ff76012c6b268b38c03bd32dd20a3a166de
Invigo <invigo.devel@gmail.com>
2018-05-11 19:55:45 +0300

    modified grade-school

--- grade-school/src/School.hs
+++ grade-school/src/School.hs
@@ -1,25 +1,40 @@
 module School (School, add, empty, grade, sorted) where
 
-import Data.List (filter, unzip, sort)
-
-type Student = (Int, [String])
-type School = [Student]
-
-
-add :: Int -> String -> School -> School
-add gradeNum student school 
-    | grade gradeNum school /= empty = [ (gradeNum, sort (student : (grade gradeNum school))) ++ filter (\(x, y) -> x /= gradeNum) school
-    | otherwise                      = [ (gradNum, [student]) ] ++ school
+import Data.Maybe (fromMaybe)
+import Data.List (sortBy, sort)
+   
+type School = [(Int, [String])]
 
 
 empty :: School
 empty = []
 
 
+add :: Int -> String -> School -> School
+add gradeNum student school = case find gradeNum school of 
+    Nothing       -> (gradeNum, [student]) : school
+    Just students -> (gradeNum, student:students) : remove gradeNum school
+
+
+remove :: Int -> School -> School
+remove _ [] = empty
+remove y (x:xs) 
+    | y == fst x = xs
+    | otherwise  = x : remove y xs
+
+
+find :: Int -> School -> Maybe [String]
+find x [] = Nothing
+find x ((y1, y2):ys) 
+    | x == y1   = Just $ sort y2
+    | otherwise = find x ys  
+
+
 grade :: Int -> School -> [String]
-grade gradeNum school 
-    | filter (\ (x, y) -> x == gradeNum) school /= empty = concat $ snd (unzip (filter (\ (x, y) -> x == gradeNum) school))
-    | otherwise                                          = []
+grade gradeNum school = fromMaybe [] (find gradeNum school)  
+
 
 sorted :: School -> [(Int, [String])]
-sorted school = sort school
+sorted = sortBy (\ x y -> compare (fst x) (fst y)) . students
+    where students [] = []
+          students ((x1, x2):xs) = (x1, sort x2) : students xs
