e036c97258c4f0e936a3a675ee81e4a88d987a5c
Invigo <invigo.devel@gmail.com>
2018-05-19 00:27:19 +0300

    fixed isbn-verifier

--- isbn-verifier/src/IsbnVerifier.hs
+++ isbn-verifier/src/IsbnVerifier.hs
@@ -1,32 +1,16 @@
 module IsbnVerifier (isbn) where
 
 import Data.Char
-import Data.Maybe
-import Control.Monad
-
--- isbnVal :: (Int, Char) -> Maybe Int
--- isbnVal (idx, d) 
---     | (9, 'X')   = Just 10 
---     | isNumber d = Just $ (digitToInt d) * (10 - idx) 
---     | otherwise  = Nothing
-
--- isbnSum :: 
--- isbnSum = fmap sum $ sequence $ map isbnValue (zip [0..])
-
 
 isbn :: String -> Bool
 isbn "" = False
--- isbn s  = length (valid s) == 10 && isbnSum `mod` 11 == 0 && 'X' `notElem` (init $ valid s) 
-isbn s = valid (filter (/='-') s)
+isbn s  = length (filter (/='-') s) == 10 && valid (filter (/='-') s)
     where
-        isbnSum xs = fmap sum $ sequence $ map isbnVal (zip [0..] xs)
-        valid  xs = case isbnSum xs of
-            Just n  -> n `mod` 11 == 0
-            Nothing -> False
-        isbnVal (9, 'X')      = Just 10
-        isbnVal (index, digit)
-            | isNumber digit   = Just $ (digitToInt digit) * (10 - index)
-            | otherwise        = Nothing
-
-
-
+    sum' xs = fmap sum $ sequence $ map isbnCh (zip [0..] xs)
+    valid  xs = case sum' xs of
+        Just n  -> n `mod` 11 == 0
+        Nothing -> False
+    isbnCh (9, 'X')  = Just 10
+    isbnCh (idx, d)
+        | isNumber d = Just $ (digitToInt d) * (10 - idx)
+        | otherwise  = Nothing
