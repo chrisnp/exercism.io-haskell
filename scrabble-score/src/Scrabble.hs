46238727356bb9827e602437917a54a711e04050
Invigo <invigo.devel@gmail.com>
2018-05-22 17:29:35 +0300

    added scrabble-score

--- scrabble-score/src/Scrabble.hs
+++ scrabble-score/src/Scrabble.hs
@@ -1,7 +1,20 @@
 module Scrabble (scoreLetter, scoreWord) where
 
+import Data.Char
+
 scoreLetter :: Char -> Integer
-scoreLetter letter = error "You need to implement this function."
+scoreLetter letter
+    | letterVal ['A', 'E', 'I', 'O', 'U'] = 1
+    | letterVal ['L', 'N', 'R', 'S', 'T'] = 1
+    | letterVal ['D', 'G']                = 2
+    | letterVal ['B', 'C', 'M', 'P']      = 3
+    | letterVal ['F', 'H', 'V', 'W', 'Y'] = 4
+    | letterVal ['K']                     = 5
+    | letterVal ['J', 'X']                = 8
+    | letterVal ['Q', 'Z']                =10
+    | otherwise                           = 0
+    where letterVal ofLetters = (toUpper letter) `elem` ofLetters
+
 
 scoreWord :: String -> Integer
-scoreWord word = error "You need to implement this function."
+scoreWord word = sum $ map scoreLetter word
