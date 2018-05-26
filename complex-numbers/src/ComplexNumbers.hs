12b82076891870ca2cc66964e17281836ba4c2de
Invigo <invigo.devel@gmail.com>
2018-05-26 18:51:45 +0300

    added complex-numbers

---
+++ complex-numbers/src/ComplexNumbers.hs
@@ -0,0 +1,47 @@
+module ComplexNumbers
+(Complex,
+ conjugate,
+ abs,
+ real,
+ imaginary,
+ mul,
+ add,
+ sub,
+ div,
+ complex) where
+
+import Prelude hiding (div, abs)
+
+-- Data definition -------------------------------------------------------------
+data Complex a = Complex a a deriving(Eq, Show)
+
+complex :: (a, a) -> Complex a
+complex (real, imag) = Complex real imag 
+
+-- unary operators -------------------------------------------------------------
+conjugate :: Num a => Complex a -> Complex a
+conjugate (Complex real imag) = Complex real ( -imag ) 
+
+abs :: Floating a => Complex a -> a
+abs (Complex real imag) = sqrt ( real ^ 2 + imag ^ 2 ) 
+
+real :: Num a => Complex a -> a
+real (Complex real _) = real 
+
+imaginary :: Num a => Complex a -> a
+imaginary (Complex _ imag) = imag 
+
+-- binary operators ------------------------------------------------------------
+mul :: Num a => Complex a -> Complex a -> Complex a
+mul (Complex re1 im1) (Complex re2 im2) = Complex (re1 * re2 - im1 * im2) (re1 * im2 + re2 * im1)
+
+add :: Num a => Complex a -> Complex a -> Complex a
+add (Complex re1 im1) (Complex re2 im2) = Complex (re1 + re2) (im1 + im2)
+
+sub :: Num a => Complex a -> Complex a -> Complex a
+sub (Complex re1 im1) (Complex re2 im2) = Complex (re1 - re2) (im1 - im2)
+
+div :: Fractional a => Complex a -> Complex a -> Complex a
+div (Complex re1 im1) (Complex re2 im2) = Complex ((re1 * re2 + im1 * im2) / d) ((im1 * re2 - im2 * re1) / d)
+    where 
+        d = re2 ^ 2 + im2 ^ 2
