1907cd969376953cb3050cb9f1e33e94391e5a08
Invigo <invigo.devel@gmail.com>
2018-06-05 17:32:36 +0300

    list-ops

---
+++ list-ops/src/ListOps.hs
@@ -0,0 +1,40 @@
+module ListOps
+  ( length
+  , reverse
+  , map
+  , filter
+  , foldr
+  , foldl'
+  , (++)
+  , concat
+  ) where
+
+import Prelude hiding
+  ( length, reverse, map, filter, foldr, (++), concat )
+
+foldl' :: (b -> a -> b) -> b -> [a] -> b
+foldl' _ z []     = z
+foldl' f z (x:xs) = z `seq` foldl' f ( f z x) xs
+
+foldr :: (a -> b -> b) -> b -> [a] -> b
+foldr _ z []     = z
+foldr f z (x:xs) = f x ( foldr f z xs )
+
+length :: [a] -> Int
+length xs = foldl' (\z _ -> z + 1) 0 xs
+
+reverse :: [a] -> [a]
+reverse xs = foldl' (flip (:)) [] xs
+
+map :: (a -> b) -> [a] -> [b]
+map f xs = foldr (\x zs -> f x:zs) [] xs
+
+filter :: (a -> Bool) -> [a] -> [a]
+filter p xs = foldr (\x zs -> if p x then x:zs else zs ) [] xs
+    -- where yes x zs = if p x then x:zs else zs 
+
+(++) :: [a] -> [a] -> [a]
+xs ++ ys = foldr (:) ys xs
+
+concat :: [[a]] -> [a]
+concat xss = foldr (++) [] xss
