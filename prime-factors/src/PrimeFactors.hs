dc7f5d38f1d96de2c0dc7d6d8d5c17a250d22d6d
Invigo <invigo.devel@gmail.com>
2018-06-07 00:09:05 +0300

    prime-factors

---
+++ prime-factors/src/PrimeFactors.hs
@@ -0,0 +1,12 @@
+module PrimeFactors (primeFactors) where
+
+factors :: Integral a => a -> [a] -> [a]
+factors 1 _  = []
+factors n [] = [n]
+factors n (x:xs) | n `mod` x == 0 = x : factors (n `quot` x) (x:xs)
+                 | otherwise      = factors n xs
+
+primeFactors :: Integral a => a -> [a]
+primeFactors n = factors n [2..(floor (sqrt $ fromIntegral n :: Double))]
+            
+
