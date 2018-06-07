3b8bd2db413b3e8acb3862a72862c2c7cad1b610
Invigo <invigo.devel@gmail.com>
2018-06-07 10:12:42 +0300

    allergies 1st iter

---
+++ allergies/src/Allergies.hs
@@ -0,0 +1,19 @@
+module Allergies (Allergen(..), allergies, isAllergicTo) where
+
+import Data.Bits (testBit)
+
+data Allergen = Eggs
+              | Peanuts
+              | Shellfish
+              | Strawberries
+              | Tomatoes
+              | Chocolate
+              | Pollen
+              | Cats
+              deriving (Enum, Eq, Show)
+
+allergies :: Int -> [Allergen]
+allergies score = filter (flip isAllergicTo score) [Eggs,Peanuts,Shellfish,Strawberries,Tomatoes,Chocolate,Pollen,Cats]
+
+isAllergicTo :: Allergen -> Int -> Bool
+isAllergicTo allergen score = testBit score (fromEnum allergen)
