9764d89ff009c2631dab5110aacc6a4d3c9cdc91
Invigo <invigo.devel@gmail.com>
2018-06-15 13:51:14 +0300

    clock

--- clock/src/Clock.hs
+++ clock/src/Clock.hs
@@ -3,31 +3,32 @@ module Clock (clockHour, clockMin, fromHourMin, toString) where
 data Clock = Clock { hours :: Int, minutes :: Int } deriving Eq
 
 instance Num Clock where
-    (Clock h1 m1) + (Clock h2 m2) = fromHourMin ( h1 + h2 ) ( m1 + m2 )
-    (Clock h1 m1) - (Clock h2 m2) = fromHourMin ( h1 - h2 ) ( m1 - m2 )
+    (+) (Clock h1 m1) (Clock h2 m2) = fromHourMin ( h1 + h2 ) ( m1 + m2 )
+    (-) (Clock h1 m1) (Clock h2 m2) = fromHourMin ( h1 - h2 ) ( m1 - m2 )
     fromInteger = fromHourMin 0 . fromIntegral 
     abs _ = undefined
     signum _ = undefined
+    (*) _ _ = undefined
 
 instance Show Clock where
     show = toString
 
 clockHour :: Clock -> Int
-clockHour (Clock hours minutes) = hours 
+clockHour clock@(Clock hours minutes) = hours 
 
 clockMin :: Clock -> Int
-clockMin (Clock hours minutes) = minutes
+clockMin clock@(Clock hours minutes) = minutes
 
 fromHourMin :: Int -> Int -> Clock
 fromHourMin hour min = Clock hour' min'
     where
-        hour' = (hour + dh) `mod` 24
-        min'  = min `mod` 60
-        dh    = min `div` 60 
+        hour'  = (hour + deltaH) `mod` 24
+        min'   = min `mod` 60
+        deltaH = min `div` 60 
 
 toString :: Clock -> String
-toString clock = padTime (clockHour clock) ++ ":" ++ padTime (clockMin clock)
+toString clock = padZero (clockHour clock) ++ ":" ++ padZero (clockMin clock)
         where
-            padTime x 
+            padZero x 
                 | x < 10    = '0': show x
                 | otherwise = show x
