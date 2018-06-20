d3ce976a9b437deb566ededeaa497e64028febfd
Invigo <invigo.devel@gmail.com>
2018-06-20 21:01:01 +0300

    pythagorean-triplets

--- pythagorean-triplet/src/Triplet.hs
+++ pythagorean-triplet/src/Triplet.hs
@@ -1,10 +1,16 @@
 module Triplet (isPythagorean, mkTriplet, pythagoreanTriplets) where
 
-isPythagorean :: (Int, Int, Int) -> Bool
-isPythagorean triplet = error "You need to implement this function."
+import Data.List ( sort )
 
-mkTriplet :: Int -> Int -> Int -> (Int, Int, Int)
-mkTriplet a b c = error "You need to implement this function."
+data PythagoreanTriplet = PythagoreanTriplet {a::Int, b::Int, c::Int} deriving (Show, Eq)
 
-pythagoreanTriplets :: Int -> Int -> [(Int, Int, Int)]
-pythagoreanTriplets minFactor maxFactor = error "You need to implement this function."
+isPythagorean :: PythagoreanTriplet -> Bool
+isPythagorean triplet = ((a triplet) ^ (2::Integer)) + ((b triplet) ^ (2::Integer)) == (c triplet) ^ (2::Integer)
+
+mkTriplet :: Int -> Int -> Int -> PythagoreanTriplet
+mkTriplet a b c = PythagoreanTriplet (head sorted) (sorted !! 1) (sorted !! 2)
+    where sorted = sort [a, b, c]
+
+pythagoreanTriplets :: Int -> Int -> [PythagoreanTriplet]
+pythagoreanTriplets minFactor maxFactor = filter isPythagorean triplets
+    where triplets =  [mkTriplet a b c | a <- [minFactor .. maxFactor], b <- [a .. maxFactor], c <- [b .. maxFactor]]
