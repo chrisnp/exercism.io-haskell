daaa6ae657a36607d21c1bbed38cbed2cef7ce12
Invigo <invigo.devel@gmail.com>
2018-07-01 17:11:57 +0300

    sieve

---
+++ sieve/src/Sieve.hs
@@ -0,0 +1,10 @@
+module Sieve (primesUpTo) where
+
+primesUpTo :: Integer -> [Integer]
+primesUpTo n = 
+    let 
+        primes = 2 : filter isPrime [3, 5..]
+        isPrime p = all ((/=) 0 . mod p) (takeWhile (\n ->  n ^ 2 <= p) primes)
+    in 
+        takeWhile (<= n) primes
+        
