2dbedebd844be6e2a5c74f186ad41bafff732cc9
Invigo <invigo.devel@gmail.com>
2018-07-03 01:11:26 +0300

    word-count

---
+++ word-count/src/WordCount.hs
@@ -0,0 +1,15 @@
+module WordCount (wordCount) where
+
+import Data.Char (toLower, isAlphaNum)
+import Data.Map (Map)
+import Data.List.Split (wordsBy)
+import Data.MultiSet (fromList, toMap)
+
+-- wordCount :: String -> [(String, Int)]
+wordCount :: String -> Map String Int
+wordCount xs = 
+    let
+        normalize ('\'':rest) = map toLower (init rest)
+        normalize cleanword   = map toLower cleanword
+    in 
+        (toMap . fromList . map normalize . wordsBy (\x -> (not . isAlphaNum) x && x /= '\'')) xs
