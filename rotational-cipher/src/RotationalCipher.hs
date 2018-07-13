ee006a043c8d86da5d1e86045c615569f661926b
Invigo <invigo.devel@gmail.com>
2018-07-14 02:40:45 +0300

    rotational-cipher passing all tests

--- rotational-cipher/src/RotationalCipher.hs
+++ rotational-cipher/src/RotationalCipher.hs
@@ -1,4 +1,12 @@
 module RotationalCipher (rotate) where
 
+convert :: Int -> Char -> Char
+convert key char 
+    | i `elem` [65..90]  = toEnum $ (i + key - 65) `mod` 26 + 65 :: Char
+    | i `elem` [97..122] = toEnum $ (i + key - 97) `mod` 26 + 97 :: Char
+    | otherwise          = char
+    where i = fromEnum char
+
+
 rotate :: Int -> String -> String
-rotate = error "You need to implement this function."
+rotate key = map $ convert key
