98adae90902ebe26294b8a9750f06b96c988d8a9
Invigo <invigo.devel@gmail.com>
2018-08-02 00:27:47 +0300

    sublist refactor all tests passing

--- sublist/src/Sublist.hs
+++ sublist/src/Sublist.hs
@@ -1,6 +1,12 @@
 module Sublist (Sublist(..), sublist) where
 
+import Data.List
+
 data Sublist = Equal | Sublist | Superlist | Unequal deriving (Eq, Show)
 
-sublist :: [a] -> [a] -> Sublist
-sublist xs ys = error "You need to implement this function."
+sublist :: (Eq a) => [a] -> [a] -> Sublist
+sublist xs ys 
+    | xs == ys             = Equal
+    | flip isInfixOf ys xs = Sublist
+    | flip isInfixOf xs ys = Superlist
+    | otherwise            = Unequal
