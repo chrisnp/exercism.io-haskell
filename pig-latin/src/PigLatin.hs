a9965635b68383548fc47ff17580985c3a851729
Invigo <invigo.devel@gmail.com>
2018-09-07 22:49:59 +0300

    pig-latin all tests passing

--- pig-latin/src/PigLatin.hs
+++ pig-latin/src/PigLatin.hs
@@ -18,26 +18,3 @@ pigLatin ('t':'h':xs)      = xs ++ "th" ++ "ay"
 pigLatin ('s':'c':'h':xs)  = xs ++ "sch" ++ "ay"
 pigLatin ('r':'h':xs)      = xs ++ "rh" ++ "ay"
 pigLatin (x:xs)            = xs ++ [x] ++ "ay"
-
--- translateWord :: String -> String
--- translateWord word = back ++ front ++ "ay" 
---     where 
---         (front, back) = splt word
-        
--- splt :: String -> (String, String)
--- splt word@(x:xs)
---     | elem x []        = ("", "") 
---     | elem x "aeiou"   = ([], word)
---     | word == ('y':'t': xs)    = ([], word)
---     | word == ('x':'r': xs)    = ([], word)
---     | word == ('q':'u': xs)    = ("qu" ++ front, back)
---     | word == (x:'q':'u':xs)   = (front ++ "qu", back)
---     | word == ('c':'h': xs)    = ("ch" ++ front, back)
---     | word == ('t':'h':'r':xs) = ("thr" ++ front, back)
---     | word == ('t':'h': xs)    = ("th" ++ front, back) 
---     | word == ('s':'c':'h':xs) = ("sch" ++ front, back)
---     | otherwise        = (x:front, back) 
---     where 
---         (front, back) = splt xs
-
-    
