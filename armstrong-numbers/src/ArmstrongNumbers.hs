ea5b8484030dc5fded27332c1a606b7f05257a0b
Invigo <invigo.devel@gmail.com>
2019-06-02 12:01:20 +0300

    armstrong-numbers solution

---
+++ armstrong-numbers/src/ArmstrongNumbers.hs
@@ -0,0 +1,15 @@
+module ArmstrongNumbers (armstrong) where
+
+
+digits :: Integral a => a -> [a]
+digits x 
+    | x < 1     = []
+    | otherwise = digits (x `div` 10) ++ [x `mod` 10]
+    
+raised :: Integral a => [a] -> [a]
+raised xs = raise xs (^ (length xs))
+    where
+        raise = flip fmap
+
+armstrong :: Integral a => a -> Bool
+armstrong = (==) <*> (sum . raised . digits)
