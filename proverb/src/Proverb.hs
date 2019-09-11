010ed07f0f1d79a17d945e53a5cd5ebfe6928147
Invigo <invigo.devel@gmail.com>
2019-09-11 23:27:29 +0300

    Create Proverb.hs

---
+++ proverb/src/Proverb.hs
@@ -0,0 +1,17 @@
+module Proverb(recite) where
+
+
+pairs :: [a] -> [(a, a)]
+pairs [] = []
+pairs xs = zip xs (tail xs)
+
+premise :: String -> String -> String
+premise = printf "For want of a %s the %s was lost."
+
+conclusion :: String -> String
+conclusio = printf "And all for the want of a %s."
+
+recite :: [String] -> String
+recite [] = ""
+recite [x] = conclusion x
+recite (x:xs) = 
