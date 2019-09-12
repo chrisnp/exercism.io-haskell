d23718215220d1a468c86656f94cbabb06e003a9
Invigo <invigo.devel@gmail.com>
2019-09-12 22:26:57 +0300

    proverb solution

--- proverb/src/Proverb.hs
+++ proverb/src/Proverb.hs
@@ -1,17 +1,21 @@
 module Proverb(recite) where
 
+import Text.Printf
+import Data.List
 
-pairs :: [a] -> [(a, a)]
-pairs [] = []
-pairs xs = zip xs (tail xs)
+-- pairs :: [a] -> [(a, a)]
+-- pairs [] = []
+-- pairs xs = zip xs (tail xs)
 
 premise :: String -> String -> String
-premise = printf "For want of a %s the %s was lost."
+premise (x, y) = printf "For want of a %s the %s was lost." x y
 
 conclusion :: String -> String
-conclusion = printf "And all for the want of a %s."
+conclusion x = printf "And all for the want of a %s." x
 
 recite :: [String] -> String
 recite [] = ""
-recite [x] = conclusion x
-recite (x:xs) = 
+recite xs = verses ++ epilogue
+    where
+        verses = unlines . map premise $ (zip xs (tail xs))
+        epilogue = conclusion $ head xs
