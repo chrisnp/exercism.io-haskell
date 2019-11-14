8662ad90f26f695f3beb35fb5e3f2c1f849971f1
Zero Delta <zerodeltahedge@gmail.com>
2019-11-15 00:48:10 +0200

    reformat transpose solution

--- transpose/src/Transpose.hs
+++ transpose/src/Transpose.hs
@@ -3,7 +3,8 @@ module Transpose (transpose) where
 import qualified Data.List (transpose)
 
 transpose :: [String] -> [String]
-transpose lines = let padded = scanr (\xs ys -> if length ys > length xs 
-                                                then xs ++ (replicate (length ys - length xs) ' ')
-                                                else xs) []
-                  in Data.List.transpose $ padded lines
+transpose lines = 
+    let padded = scanr (\xs ys -> if length ys > length xs 
+                                  then xs ++ (replicate (length ys - length xs) ' ')
+                                  else xs) []
+    in Data.List.transpose $ padded lines
