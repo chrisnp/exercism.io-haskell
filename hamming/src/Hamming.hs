fa2694d8b8745d4e41b7781c7b7fb82efbcfae63
Invigo <invigo.devel@gmail.com>
2019-11-26 17:57:18 +0200

    Reformat Hamming.hs (tested)

--- hamming/src/Hamming.hs
+++ hamming/src/Hamming.hs
@@ -2,8 +2,6 @@ module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
 distance xs ys 
-    | length xs == length ys = Just $ length (filter not (zipWith (==) xs ys))
-    | otherwise              = Nothing
-
-
-
+    | length xs == length ys = 
+        Just $ length (filter not (zipWith (==) xs ys))
+    | otherwise = Nothing
