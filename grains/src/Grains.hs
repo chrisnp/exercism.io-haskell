b31ee14b9a2ded48d76d195df23214cc4430ac82
Invigo <invigo.devel@gmail.com>
2019-12-05 01:13:44 +0200

    Update Grains.hs

--- grains/src/Grains.hs
+++ grains/src/Grains.hs
@@ -3,8 +3,9 @@ module Grains (square, total) where
 import Data.Maybe
 
 square :: Integer -> Maybe Integer
-square n = if 1 <= n && n <= 64 then Just $ (2 ^ (n-1)) else Nothing
+square n = if 1 <= n && n <= 64 
+           then Just $ (2 ^ (n-1)) 
+           else Nothing
 
 total :: Integer
 total = 2^64 - 1
-        
