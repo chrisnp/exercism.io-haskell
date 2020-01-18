05246fb13038367d2261a0fdbc76ec9b088b3401
Invigo <invigo.devel@gmail.com>
2020-01-18 22:44:34 +0200

    Update Anagram.hs

--- anagram/src/Anagram.hs
+++ anagram/src/Anagram.hs
@@ -4,12 +4,13 @@ import Data.Char (toLower)
 import Data.List (sort)
 
 anagramsFor :: String -> [String] -> [String]
-anagramsFor xs xss = filter (anagramOf xs) xss
+anagramsFor xs xss = 
+    filter (anagramOf xs) xss
 
 anagramOf :: String -> String -> Bool
 anagramOf a b 
     | lower a /= lower b && sorted a == sorted b = True
-    | otherwise = False  
-    where 
+    | otherwise = False   
+    where
         lower = map toLower
-        sorted = sort . lower
+        sorted = sort . lower    
