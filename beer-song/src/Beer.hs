bf56ec1fd09949a8cbba337d9870c888c08255fd
Invigo <invigo.devel@gmail.com>
2020-01-23 12:14:46 +0200

    Refactor Beer.hs

--- beer-song/src/Beer.hs
+++ beer-song/src/Beer.hs
@@ -1,24 +1,33 @@
 module Beer (song) where
 
 
-beers :: Int -> String
-beers n 
+bottles :: Int -> String
+bottles n 
     | n == 0    = "no more bottles"
     | n == 1    = "1 bottle"
     | otherwise = show n ++ " bottles"
 
 
+
 verse :: Int -> String
 verse n
     | n == 0    = "No more bottles of beer on the wall, no more bottles of beer.\n\
-                \Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-    | otherwise = beers n ++ " of beer on the wall, " ++ beers n ++ " of beer.\n\
-                \Take " ++ down n ++ " down and pass it around, " ++ beers (n-1) ++ " of beer on the wall.\n\
-                \\n"
+                  \Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    | otherwise = bottles n ++ 
+                  " of beer on the wall, " ++
+                  bottles n ++ 
+                  " of beer.\n\
+                  \Take " ++ 
+                  down n ++ 
+                  " down and pass it around, " ++ 
+                  bottles (n-1) ++ 
+                  " of beer on the wall.\n\
+                  \\n"
     where 
-        down n 
-            | n == 1    = "it"
+        down x 
+            | x == 1    = "it"
             | otherwise = "one"
+
 
 song :: String
 song = foldl (\acc x -> verse x ++ acc) "" [0..99]
