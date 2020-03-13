4bfb3f163b546d2988402c4cb001c42a52d61af9
Invigo <invigo.devel@gmail.com>
2020-03-14 01:56:49 +0200

    Reformat CollatzConjecture

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -4,10 +4,15 @@ import Data.List
 
 collatzStep :: Integer -> Integer
 collatzStep num 
-    | even num  = num `quot` 2 
-    | otherwise = (3 * num ) + 1
+    | even num  = 
+        num `quot` 2 
+    | otherwise = 
+        (3 * num ) + 1
 
 collatz :: Integer -> Maybe Integer
 collatz num  
-    | num > 0 = toInteger <$> ( elemIndex 1 $ iterate collatzStep num ) 
-    | otherwise = Nothing 
+    | num > 0 = 
+        toInteger <$> 
+        ( elemIndex 1 $ iterate collatzStep num ) 
+    | otherwise = 
+        Nothing 
