ad17d4b9b569c91f841de92321855fa7152f7345
Invigo <invigo.devel@gmail.com>
2020-03-14 01:57:39 +0200

    Reformat SumOfMultiples

--- sum-of-multiples/src/SumOfMultiples.hs
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -3,18 +3,25 @@ module SumOfMultiples (sumOfMultiples) where
 import Data.List(nub)
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples [] limit      = 0
+sumOfMultiples [] limit = 
+    0
 sumOfMultiples factors limit = 
     sum $ distinctFactors factors limit
 
-distinctFactors :: [Integer] -> Integer -> [Integer]
-distinctFactors [] limit     = []
+distinctFactors :: (Integral a) => [a] -> a -> [a]
+distinctFactors [] limit = 
+    []
 distinctFactors (x:xs) limit = 
-    nub $ (distinctFactors xs limit) ++ (appendFactor x limit 1)  
+    nub $ 
+    (distinctFactors xs limit) ++ 
+    (appendFactor x limit 1)  
 
-appendFactor :: Integer -> Integer -> Integer -> [Integer]
+appendFactor :: (Integral a) => a -> a -> a -> [a]
 appendFactor factor limit index 
-    | factor * index >= limit = []
-    | factor == 0 = []
+    | factor * index >= limit = 
+        []
+    | factor == 0 = 
+        []
     | otherwise = 
-        (factor * index) : appendFactor factor limit (index + 1)
+        (factor * index) : 
+        appendFactor factor limit (index + 1)
