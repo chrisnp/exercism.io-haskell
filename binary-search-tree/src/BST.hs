569a6f630beeb4aaea4535d14b26100e653ecb50
Invigo <invigo.devel@gmail.com>
2020-04-11 17:40:11 +0300

    Refactor BST.hs

--- binary-search-tree/src/BST.hs
+++ binary-search-tree/src/BST.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveFoldable #-}
+
 module BST
     ( BST
     , bstLeft
@@ -10,45 +12,75 @@ module BST
     , toList
     ) where
 
-import Data.Foldable ( foldl', toList )
+
+import Data.Foldable ( Foldable, 
+                       foldl' )
 
 
-data BST a = Empty | BST (BST a) a (BST a) deriving (Eq, Show)
+data BST a = Empty | BST (BST a) a (BST a) 
+             deriving (Eq, Show, Foldable)
 
-instance Foldable BST where
-    foldMap f Empty = mempty
-    foldMap f (BST left val right) = foldMap f left `mappend` f val `mappend` foldMap f right
 
-    
 bstLeft :: BST a -> Maybe (BST a)
-bstLeft Empty = Nothing
-bstLeft (BST left _ _)  = Just left
+bstLeft tree = 
+    let 
+        (BST left _ _) = tree
+    in 
+        Just left
+
 
 bstRight :: BST a -> Maybe (BST a)
-bstRight Empty = Nothing
-bstRight (BST _ _ right)  = Just right
+bstRight tree = 
+    let 
+        (BST _ _ right) = tree
+    in 
+        Just right
+
 
 bstValue :: BST a -> Maybe a
-bstValue Empty = Nothing
-bstValue (BST _ val _)  = Just val
+bstValue Empty = 
+    Nothing
+bstValue tree = 
+    let
+        (BST _ val _) = tree
+    in 
+        Just val
+
 
 empty :: BST a
 empty = Empty
 
+
 fromList :: Ord a => [a] -> BST a
-fromList xs = foldl' (flip insert) empty xs
+fromList xs = 
+    foldl' (flip insert) empty xs
+
 
 insert :: Ord a => a -> BST a -> BST a
-insert x Empty = singleton x
-insert x (BST left val right) 
-        | x <= val  = BST (insert x left) val right
-        | otherwise = BST left val (insert x right)
+insert x Empty =
+    singleton x
+insert x tree 
+    | x <= val  = 
+        BST (insert x left) val right
+    | otherwise =
+        BST left val (insert x right)    
+    where 
+        (BST left val right) = tree
+
 
 singleton :: a -> BST a
-singleton x = BST Empty x Empty
+singleton x = 
+    BST Empty x Empty
 
--- We do not need this any more, as the toList from Foldable is used 
--- after defining the Foldable Monoid instance above
--- toList :: BST a -> [a]
--- toList Empty                = []
--- toList (BST left val right) = (toList left) ++ [val] ++ (toList right)
+
+toList :: BST a -> [a]
+toList Empty = 
+    []
+toList tree = 
+    let 
+        (BST left val right) = tree
+    in 
+        (toList left) ++ 
+        [val] ++ 
+        (toList right)
+        
