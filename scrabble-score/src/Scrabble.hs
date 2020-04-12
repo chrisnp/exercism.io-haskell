0f43d2bed49bea93507c0d8c1d6ae087d5f05cc5
Invigo <invigo.devel@gmail.com>
2020-04-13 01:11:07 +0300

    Refactor Scrabble.hs

--- scrabble-score/src/Scrabble.hs
+++ scrabble-score/src/Scrabble.hs
@@ -5,20 +5,20 @@ import Data.Char
 
 scoreLetter :: Char -> Integer
 scoreLetter letter
-    | letterVal ['A', 'E', 'I', 'O', 'U'] = 1
-    | letterVal ['L', 'N', 'R', 'S', 'T'] = 1
-    | letterVal ['D', 'G']                = 2
-    | letterVal ['B', 'C', 'M', 'P']      = 3
-    | letterVal ['F', 'H', 'V', 'W', 'Y'] = 4
-    | letterVal ['K']                     = 5
-    | letterVal ['J', 'X']                = 8
-    | letterVal ['Q', 'Z']                = 10
-    | otherwise                           = 0
+    | letterVal "AEIOU" = 1
+    | letterVal "LNRST" = 1
+    | letterVal "DG"    = 2
+    | letterVal "BCMP"  = 3
+    | letterVal "FHVWY" = 4
+    | letterVal "K"     = 5
+    | letterVal "JX"    = 8
+    | letterVal "QZ"    = 10
+    | otherwise         = 0
     where 
-        letterVal ofLetters = 
-                (toUpper letter) `elem` ofLetters
+        letterVal = 
+            (elem . toUpper) letter
 
 
 scoreWord :: String -> Integer
-scoreWord word = 
-    sum $ map scoreLetter word
+scoreWord = 
+    sum . fmap scoreLetter 
