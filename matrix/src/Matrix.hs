4e839170c6e8d18850b7cfd488d4c774be384032
Invigo <invigo.devel@gmail.com>
2020-04-16 22:50:50 +0300

    Refactor Matrix.hs

--- matrix/src/Matrix.hs
+++ matrix/src/Matrix.hs
@@ -22,37 +22,54 @@ data Matrix a = Matrix {
                 } deriving (Show, Eq) 
 
 row :: Int -> Matrix a -> Vector a
-row x matrix = V.take (cols matrix) $ snd $ V.splitAt (x * (cols matrix)) $ vect matrix
-                
--- rows :: Matrix a -> Int
--- rows matrix = rows matrix -- error "You need to implement this function."
-
--- cols :: Matrix a -> Int
--- cols matrix = error "You need to implement this function."
+row x matrix = 
+    V.take (cols matrix) 
+    $ snd 
+    $ V.splitAt (x * (cols matrix)) 
+    $ vect matrix
 
 column :: Int -> Matrix a -> Vector a
-column x matrix = V.fromList $ takeNth (x + 1) $ V.toList $ vect matrix
+column x matrix = 
+    V.fromList 
+    $ takeNth (x + 1) 
+    $ V.toList 
+    $ vect matrix
     where 
-        takeNth n a = case drop (n - 1) a of 
-            (y:ys) -> y : takeNth (cols matrix) ys
-            []     -> []      
+        takeNth n a = 
+            case drop (n - 1) a of 
+                (y:ys) -> 
+                    y : takeNth (cols matrix) ys
+                [] -> 
+                    []      
 
 flatten :: Matrix a -> Vector a
-flatten matrix = vect matrix
+flatten = 
+    vect
 
 fromList :: [[a]] -> Matrix a
-fromList []  = Matrix V.empty 0 0
-fromList xss = Matrix (V.fromList $ concat xss) (length xss) (length $ head xss) 
+fromList []  = 
+    Matrix V.empty 0 0
+fromList xss = 
+    Matrix (V.fromList $ concat xss) 
+           (length xss) 
+           (length $ head xss) 
 
 fromString :: Read a => String -> Matrix a
-fromString xs = (fromList . map (map read . words) . lines) xs
+fromString = 
+    fromList . map (map read . words) . lines
 
 reshape :: (Int, Int) -> Matrix a -> Matrix a
-reshape dimensions matrix = Matrix { rows = fst dimensions, cols = snd dimensions, vect = vect matrix }
+reshape dimensions matrix = 
+    Matrix { rows = fst dimensions, 
+             cols = snd dimensions, 
+             vect = vect matrix }
 
 
 shape :: Matrix a -> (Int, Int)
-shape matrix = (rows matrix, cols matrix)
+shape matrix = 
+    liftM2 (,) rows cols
 
 transpose :: Matrix a -> Matrix a
-transpose matrix = fromList $ [V.toList $ column (c - 1) matrix | c <- [1..(cols matrix)]]
+transpose matrix = 
+    fromList 
+    $ [V.toList $ column (c - 1) matrix | c <- [1..(cols matrix)]]
