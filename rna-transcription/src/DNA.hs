f0f492d704499fd4597eda23bbd06e16fbfe8c90
Invigo <invigo.devel@gmail.com>
2020-04-22 20:27:06 +0300

    Refactor DNA.hs (rna-transcription)

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -1,11 +1,12 @@
 module DNA (toRNA) where
 
 toRNA :: String -> Either Char String
--- toRNA [] = Nothing
-toRNA xs = traverse rnaComplement xs
-    where
+toRNA = 
+    let 
         rnaComplement 'G' = Right 'C'
         rnaComplement 'C' = Right 'G'
         rnaComplement 'T' = Right 'A'
         rnaComplement 'A' = Right 'U'
         rnaComplement  n  = Left n
+    in
+        traverse rnaComplement
