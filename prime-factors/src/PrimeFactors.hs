0178fc7bd8fb84a6d2e97bbe259665d4faca4a41
Invigo <invigo.devel@gmail.com>
2020-05-07 16:48:43 +0300

    Refactor prime-factors

--- prime-factors/src/PrimeFactors.hs
+++ prime-factors/src/PrimeFactors.hs
@@ -1,12 +1,19 @@
 module PrimeFactors (primeFactors) where
 
 factors :: Integral a => a -> [a] -> [a]
-factors 1 _  = []
-factors n [] = [n]
-factors n (x:xs) | n `mod` x == 0 = x : factors (n `quot` x) (x:xs)
-                 | otherwise      = factors n xs
+factors 1 _  = 
+    []
+factors n [] = 
+    [n]
+factors n (x:xs) 
+    | n `mod` x == 0 = 
+        x : factors (n `quot` x) (x:xs)
+    | otherwise = 
+        factors n xs
 
 primeFactors :: Integral a => a -> [a]
-primeFactors n = factors n [2..(floor (sqrt $ fromIntegral n :: Double))]
-            
-
+primeFactors = 
+    (<*>) factors ( enumFromTo 2 
+                    . floor 
+                    . sqrt 
+                    . fromIntegral )
