3afacff1a9c87f79da74d6e68e7641d9b2fcf062
Invigo <invigo.devel@gmail.com>
2020-06-22 23:13:58 +0300

    Reformat isbn-verifier

--- isbn-verifier/src/IsbnVerifier.hs
+++ isbn-verifier/src/IsbnVerifier.hs
@@ -6,13 +6,17 @@ isbn :: String -> Bool
 isbn "" = False
 isbn s  = 
     let
-        sum' xs = fmap sum $ sequence $ map isbnCh (zip [0..] xs)
+        sum' xs = 
+            fmap sum $ sequence $ map isbnCh (zip [0..] xs)
         valid  xs = case sum' xs of
             Just n  -> n `mod` 11 == 0
             Nothing -> False
         isbnCh (9, 'X')  = Just 10
         isbnCh (idx, d)
-            | isNumber d = Just $ (digitToInt d) * (10 - idx)
-            | otherwise  = Nothing
+            | isNumber d = 
+                Just $ (digitToInt d) * (10 - idx)
+            | otherwise  = 
+                Nothing
     in 
-        length (filter (/='-') s) == 10 && valid (filter (/='-') s)
+        length (filter (/='-') s) == 10 
+        && valid (filter (/='-') s)
