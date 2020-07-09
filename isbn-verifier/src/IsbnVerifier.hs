f4582af2e2768d91225e963f584f8aa94d174fb4
Invigo <invigo.devel@gmail.com>
2020-07-09 13:11:51 +0300

    Refactor IsbnVerifier.hs

--- isbn-verifier/src/IsbnVerifier.hs
+++ isbn-verifier/src/IsbnVerifier.hs
@@ -4,11 +4,11 @@ import Data.Char
 
 isbn :: String -> Bool
 isbn "" = False
-isbn s  = 
+isbn s = 
     let
-        sum' xs = 
-            fmap sum $ sequence $ map isbnCh (zip [0..] xs)
-        valid  xs = case sum' xs of
+        csum = 
+            fmap sum . sequence . map isbnCh .zip [0..]
+        valid xs = case csum xs of
             Just n  -> n `mod` 11 == 0
             Nothing -> False
         isbnCh (9, 'X')  = Just 10
@@ -18,5 +18,5 @@ isbn s  =
             | otherwise  = 
                 Nothing
     in 
+        valid (filter (/='-') s) && 
         length (filter (/='-') s) == 10 
-        && valid (filter (/='-') s)
