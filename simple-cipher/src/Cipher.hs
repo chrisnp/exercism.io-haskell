e6c6ac63ffe6130399d2132963a13aa619b1fe43
Invigo <invigo.devel@gmail.com>
2020-07-14 19:58:13 +0300

    Update Cipher.hs

--- simple-cipher/src/Cipher.hs
+++ simple-cipher/src/Cipher.hs
@@ -3,13 +3,16 @@ module Cipher (caesarDecode, caesarEncode, caesarEncodeRandom) where
 import System.Random
 
 shift :: (Int, Char) -> Char
-shift (k, v) = toEnum $ (fromEnum 'a') + mod (fromEnum v + k - (fromEnum 'a')) 26
+shift (k, v) = 
+    toEnum $ (fromEnum 'a') + mod (fromEnum v + k - (fromEnum 'a')) 26
 
 caesarDecode :: String -> String -> String
-caesarDecode key encodedText = (map shift . zip (cycle (map ((-) (fromEnum 'a') . fromEnum) key))) encodedText
+caesarDecode = 
+    (map shift .) . zip . cycle . map ((-) (fromEnum 'a') . fromEnum)
 
 caesarEncode :: String -> String -> String
-caesarEncode key text = (map shift . zip (cycle (map (flip (-) (fromEnum 'a') . fromEnum) key))) text
+caesarEncode = 
+    (map shift .) . zip . cycle . map (subtract (fromEnum 'a') . fromEnum)
 
 caesarEncodeRandom :: String -> IO (String, String)
 caesarEncodeRandom text = do
