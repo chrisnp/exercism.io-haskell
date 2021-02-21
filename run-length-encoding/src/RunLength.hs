e30fa7364ac9023fd0a091b4c420da7a2d99dfb1
Invigo <invigo.devel@gmail.com>
2021-02-22 00:42:09 +0200

    Refactor RunLength.hs

--- run-length-encoding/src/RunLength.hs
+++ run-length-encoding/src/RunLength.hs
@@ -3,26 +3,23 @@ module RunLength (decode, encode) where
 import Data.Char
 
 decode :: String -> String
-decode encodedText
-    | encodedText == "" = 
-        ""
-    | otherwise = 
+decode "" = ""
+decode encoded =
+    let 
+        count = if null num then 1 else read num
+        num = takeWhile isDigit encoded
+        char = head (filter (not . isDigit) encoded)
+    in
         replicate count char ++ 
-        (decode $ drop (1 + length num) encodedText) 
-    where 
-        count  = if null num then 1 else read num
-        num    = takeWhile isDigit encodedText
-        char   = head $ filter (not . isDigit) 
-                                encodedText
-
+        decode (drop (1 + length num) encoded)
 
 encode :: String -> String
-encode text 
-    | text == "" = 
-        ""
-    | otherwise  = 
-        lenStr ++ char : (encode $ drop len text)
-    where 
-        len    = length $ takeWhile (== char) text
+encode "" = ""
+encode text =
+    let 
+        len    = length (takeWhile (char ==) text)
         char   = head text
         lenStr = if len == 1 then "" else show len
+    in
+        lenStr ++ char : encode (drop len text)
+        
