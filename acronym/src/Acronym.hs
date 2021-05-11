4b47245f4e7dd8889a5087ce5b1299633eda4d6b
Invigo <invigo.devel@gmail.com>
2021-05-11 17:52:42 +0300

    Reformat and retest acronym

--- acronym/src/Acronym.hs
+++ acronym/src/Acronym.hs
@@ -6,15 +6,10 @@ import Data.Maybe
 
 isFirst :: (Char, Char) -> Maybe Char
 isFirst (x, y)
-    | x == '\'' = 
-        Nothing
-    | (not . isLetter) x && isLetter y = 
-        Just $ toUpper y
-    | (not . isUpper) x  && isUpper y = 
-        Just y
-    | otherwise = 
-        Nothing
+    | x == '\'' = Nothing
+    | (not . isLetter) x && isLetter y = Just $ toUpper y
+    | (not . isUpper) x  && isUpper y = Just y
+    | otherwise = Nothing
 
 abbreviate :: String -> String
-abbreviate =
-        mapMaybe isFirst . (zip =<< (' ' :))
+abbreviate = mapMaybe isFirst . (zip =<< (' ' :))
