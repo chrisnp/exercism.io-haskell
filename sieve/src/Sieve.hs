f8d3c7292cb925b7326e92d8c7838f1a042f1301
Invigo <invigo.devel@gmail.com>
2021-09-07 23:27:36 +0300

    Refactor Sieve.hs

--- sieve/src/Sieve.hs
+++ sieve/src/Sieve.hs
@@ -3,14 +3,16 @@ module Sieve (primesUpTo) where
 
 primesUpTo :: Integer -> [Integer]
 primesUpTo n
-  | n < 2 = []
-  | otherwise = 2 : sieve [3,5 .. n]
+  | n <= 1 = []
+  | otherwise = 2 : sieve [3, 5 .. n]
 
 sieve :: [Integer] -> [Integer]
 sieve [] = []
 sieve (x:xs) = 
     let 
-        isMultiple p q = 
-            (or . map (== q)) [p, p+p .. q] 
+        multiples n upto = 
+            [ 2*n, 3*n .. upto ]
+        productOf d m = 
+            (or . map (== m)) (d : multiples d m) 
     in
-        x:sieve (filter (not . isMultiple x) xs)
+        x : sieve (filter (not . productOf x) xs)
