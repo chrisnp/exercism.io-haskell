1040cdf1054c71be2547cbc461b8e0198d58c982
Invigo <invigo.devel@gmail.com>
2022-03-25 22:31:30 +0200

    Refactor Clock.hs

--- clock/src/Clock.hs
+++ clock/src/Clock.hs
@@ -1,19 +1,12 @@
-module Clock (fromHourMin,
-              addDelta,
-              toString) 
-    where
+module Clock (fromHourMin, addDelta, toString) where
 
 data Clock = 
-    Clock { hours :: Int, minutes :: Int } 
-    deriving Eq
+    Clock { hours :: Int, minutes :: Int } deriving Eq
 
 instance Num Clock where
-    (Clock h1 m1) + (Clock h2 m2) = 
-        fromHourMin ( h1 + h2 ) ( m1 + m2 )
-    (Clock h1 m1) - (Clock h2 m2) = 
-        fromHourMin ( h1 - h2 ) ( m1 - m2 )
-    fromInteger = 
-        fromHourMin 0 . fromIntegral 
+    (Clock h1 m1) + (Clock h2 m2) = fromHourMin ( h1 + h2 ) ( m1 + m2 )
+    (Clock h1 m1) - (Clock h2 m2) = fromHourMin ( h1 - h2 ) ( m1 - m2 )
+    fromInteger = fromHourMin 0 . fromIntegral 
     abs _ = undefined
     signum _ = undefined
 
@@ -27,11 +20,13 @@ clockMin :: Clock -> Int
 clockMin (Clock hours minutes) = minutes
 
 fromHourMin :: Int -> Int -> Clock
-fromHourMin hour min = Clock h m
-    where
+fromHourMin hour min = 
+    let
+        dh = min `div` 60
         h  = (hour + dh) `mod` 24
         m  = min `mod` 60
-        dh = min `div` 60 
+    in
+        Clock h m
 
 addDelta :: Int -> Int -> Clock -> Clock
 addDelta hour min (Clock h total) = 
@@ -39,10 +34,9 @@ addDelta hour min (Clock h total) =
 
 toString :: Clock -> String
 toString clock = 
-    padTime (clockHour clock) ++ ":" ++ padTime (clockMin clock)
-        where
-            padTime x 
-                | x < 10    = '0': show x
-                | otherwise = show x
-
--- updated hastily, to conform to new specification
+    let 
+        padTime x 
+            | x < 10    = '0': show x
+            | otherwise = show x
+    in
+        padTime (clockHour clock) ++ ":" ++ padTime (clockMin clock)
