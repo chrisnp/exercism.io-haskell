b4b442d670b26c251f627f5585d58e60a27b139c
Invigo <invigo.devel@gmail.com>
2022-03-29 18:38:41 +0300

    Reformat SecretHandshake.hs

--- secret-handshake/src/SecretHandshake.hs
+++ secret-handshake/src/SecretHandshake.hs
@@ -3,21 +3,11 @@ module SecretHandshake (handshake) where
 import Data.Bits (bit, (.&.))
 
 actions :: [String]
-actions = 
-    [
-     "wink", 
-     "double blink", 
-     "close your eyes", 
-     "jump"
-    ]
+actions = [ "wink", "double blink", "close your eyes", "jump" ]
 
 handshake :: Int -> [String]
 handshake n 
-    | bit 4 .&. n == 0 = 
-        events
-    | otherwise = 
-        reverse events
+    | bit 4 .&. n == 0 = events
+    | otherwise = reverse events
     where
-        events = 
-            [actions !! i | i <- [0..3], 
-                            bit i .&. n /= 0]
+        events = [actions !! i | i <- [0..3], bit i .&. n /= 0]
