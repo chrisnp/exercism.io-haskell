f3a00c35d66b45e9efe6d3327bf6aa95eb065f9c
Invigo <invigo.devel@gmail.com>
2022-11-25 23:09:32 +0200

    Refactor palindrome-products/src/Palindromes.hs

--- palindrome-products/src/Palindromes.hs
+++ palindrome-products/src/Palindromes.hs
@@ -1,5 +1,25 @@
 module Palindromes (largestPalindrome, smallestPalindrome) where
 
+largestPalindrome :: Integer -> Integer -> Maybe (Integer, [(Integer, Integer)])
+largestPalindrome minFactor maxFactor =  
+    Just (largest, findFactors largest minFactor maxFactor)
+    where
+        factors = [ x * y |
+                    x <- [minFactor .. maxFactor],
+                    y <- [x .. maxFactor] ]
+        largest = maximum $ filter isPalindrome factors
+
+smallestPalindrome :: Integer -> Integer -> Maybe (Integer, [(Integer, Integer)])
+smallestPalindrome minFactor maxFactor = 
+    Just (smallest, findFactors smallest minFactor maxFactor)
+    where 
+        factors = [ x * y | 
+                    x <- [minFactor .. maxFactor],
+                    y <- [x .. maxFactor] ]
+        smallest = minimum $ filter isPalindrome factors
+
+-- Auxiliary 
+
 reversed :: Integer -> Integer
 reversed num = foldl (\acc x -> 10 * acc + x) 0 (digits num) 
     where
@@ -15,24 +35,11 @@ palindromeProduct :: (Integer, Integer) -> Bool
 palindromeProduct = isPalindrome . uncurry (*)
 
 findFactors :: Integer  -> Integer  -> Integer  -> [(Integer , Integer )]
-findFactors number minFactor maxFactor = [ (x, div number x) | x <- smallest, maxFactor >= div number x]
+findFactors number minFactor maxFactor = 
+    [ (x, div number x) | x <- smallest, maxFactor >= div number x]
     where
-        limit = min maxFactor (floor $ (sqrt :: Double -> Double) $ fromIntegral number)
-        smallest = [y | y <- [ minFactor .. limit ], 0 == mod number y]
+        limit = 
+            min maxFactor (floor $ (sqrt :: Double -> Double) $ fromIntegral number)
+        smallest = 
+            [y | y <- [ minFactor .. limit ], 0 == mod number y]
 
-largestPalindrome :: Integer -> Integer -> (Integer, [(Integer, Integer)])
-largestPalindrome minFactor maxFactor = (largest, findFactors largest minFactor maxFactor)
-    where
-        factors = [ x * y |
-                    x <- [minFactor .. maxFactor],
-                    y <- [x .. maxFactor] ]
-        largest = maximum $ filter isPalindrome factors
-
-smallestPalindrome :: Integer -> Integer -> (Integer, [(Integer, Integer)])
-smallestPalindrome minFactor maxFactor = (smallest, findFactors smallest minFactor maxFactor)
-    where 
-        factors = [ x * y | 
-                    x <- [minFactor .. maxFactor],
-                    y <- [x .. maxFactor] ]
-        smallest = minimum $ filter isPalindrome factors
-      
