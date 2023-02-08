1aa97d2b51bfc8642e5976704c1fa68cad1b4a43
Invigo <invigo.devel@gmail.com>
2023-02-08 13:19:27 +0200

    Update Beer.hs

--- beer-song/src/Beer.hs
+++ beer-song/src/Beer.hs
@@ -8,11 +8,12 @@ bottles n
     | otherwise = show n ++ " bottles"
 
 
-
 verse :: Int -> String
 verse n
-    | n == 0    = "No more bottles of beer on the wall, no more bottles of beer.\n\
-                  \Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    | n == 0    = "No more bottles of beer on the wall," ++ 
+                  "no more bottles of beer.\n" ++
+                  "\Go to the store and buy some more," ++ 
+                  "99 bottles of beer on the wall.\n"
     | otherwise = bottles n ++ 
                   " of beer on the wall, " ++
                   bottles n ++ 
@@ -27,7 +28,6 @@ verse n
         down x 
             | x == 1    = "it"
             | otherwise = "one"
-
 
 song :: String
 song = foldl (\acc x -> verse x ++ acc) "" [0..99]
