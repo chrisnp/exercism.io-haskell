d5a3f791cc1b66f42f2ece2c6a2c875894b019b6
Invigo <invigo.devel@gmail.com>
2023-11-27 03:00:26 +0200

    Update Tests.hs

--- binary-search-tree/test/Tests.hs
+++ binary-search-tree/test/Tests.hs
@@ -1,5 +1,5 @@
-import Test.Hspec        (Spec, it, shouldBe)
-import Test.Hspec.Runner (configFastFail, defaultConfig, hspecWith)
+import Test.Hspec        (Spec, it, shouldBe, shouldNotBe)
+import Test.Hspec.Runner (configFailFast, defaultConfig, hspecWith)
 
 import BST
   ( bstLeft
@@ -13,7 +13,7 @@ import BST
   )
 
 main :: IO ()
-main = hspecWith defaultConfig {configFastFail = True} specs
+main = hspecWith defaultConfig {configFailFast = True} specs
 
 specs :: Spec
 specs = do
@@ -38,6 +38,11 @@ specs = do
       let t = insert 5 (singleton int4)
       bstValue t `shouldBe` Just 4
       (bstRight t >>= bstValue) `shouldBe` Just 5
+
+    it "retrieves branches even when they are empty" $ do
+      let t = singleton int4
+      bstLeft t `shouldNotBe` Nothing
+      bstRight t `shouldNotBe` Nothing
 
     it "empty list to tree" $
       fromList noInts `shouldBe` empty
@@ -76,5 +81,3 @@ specs = do
 
     it "iterating over complex tree" $
       toList (fromList [int4, 2, 1, 3, 6, 7, 5]) `shouldBe` [1..7]
-
--- a2630b9bd87e31fa939d9df48f67688bb41b0882
