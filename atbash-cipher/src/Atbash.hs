891a60af792d8dd7dbca0623bb6d25dc9763e177
Invigo <invigo.devel@gmail.com>
2023-11-27 03:00:09 +0200

    Reformat Atbash

--- atbash-cipher/src/Atbash.hs
+++ atbash-cipher/src/Atbash.hs
@@ -1,8 +1,6 @@
 module Atbash (decode, encode) where
-
 import Data.Char
 import Control.Monad (liftM2)
-
 
 encode :: String -> String
 encode = 
@@ -12,21 +10,17 @@ encode =
         chunkify 5 . map atbash . filter valid
 
 decode :: String -> String
-decode = 
-    map atbash . filter (liftM2 (||) isAlpha isDigit)
+decode = map atbash . filter (liftM2 (||) isAlpha isDigit)
 
 chunkify :: Int -> String -> String
 chunkify x text = 
     let 
         (chunk, rest) = splitAt x text
     in 
-        if null rest then 
-            chunk 
-        else 
-            chunk ++ " " ++ chunkify x rest
+        if null rest then chunk 
+        else chunk ++ " " ++ chunkify x rest
 
 atbash :: Char -> Char
 atbash x
-    | isAlpha x = 
-        chr . (-) (ord 'a' + ord 'z') . ord . toLower $ x
-    | otherwise = x
+    | isAlpha x = chr . (-) (ord 'a' + ord 'z') . ord . toLower $ x
+    | otherwise = id x
