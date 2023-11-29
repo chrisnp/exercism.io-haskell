aa3dc211f5d9cfd3468b4b94944eecfe0d9b5906
Invigo <invigo.devel@gmail.com>
2023-11-29 04:55:43 +0200

    Update PerfectNumbers.hs

--- perfect-numbers/src/PerfectNumbers.hs
+++ perfect-numbers/src/PerfectNumbers.hs
@@ -9,25 +9,22 @@ data Classification = Deficient |
 
 classify :: Int -> Maybe Classification
 classify n 
-    | n <= 0 = Nothing
+    | n <= 0        = Nothing
     | aliquot == n  = Just Perfect
     | aliquot > n   = Just Abundant
     | otherwise     = Just Deficient
-        where 
-            aliquot = aliquotSum n
+    where 
+        aliquot = aliquotSum n
 
 factors :: Int -> [Int]
 factors n = 
     let
         semiFactors x = 
             takeWhile (\y -> y * y <= x) 
-                      [z | z <- [2..x-1], 
-                                x `mod` z == 0]
+                      [z | z <- [2..x-1], x `mod` z == 0]
         xs = semiFactors n
     in
         nub (1 : (xs ++ (map (n `div`) xs)))
-        
-                
 
 aliquotSum :: Int -> Int
 aliquotSum n 
