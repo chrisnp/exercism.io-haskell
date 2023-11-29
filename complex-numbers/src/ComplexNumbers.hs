3d8d835f64b5838c3b849e2ae7297b0e2202c53c
Invigo <invigo.devel@gmail.com>
2023-11-29 04:18:36 +0200

    Reformat ComplexNumbers

--- complex-numbers/src/ComplexNumbers.hs
+++ complex-numbers/src/ComplexNumbers.hs
@@ -1,62 +1,50 @@
 module ComplexNumbers
-(Complex,
- conjugate,
- abs,
- real,
- imaginary,
- mul,
- add,
- sub,
- div,
- exp,
- complex) where
+    (
+        Complex, complex, real, imaginary,
+        abs, conjugate, mul, add, sub, div, exp
+    ) 
+where
 
 import Prelude hiding (div, abs, exp)
 import qualified Prelude as Std
 
 -- data definition -----------------------------------------
-data Complex a = Complex a a 
-                 deriving(Eq, Show)
+data Complex a = Complex a a deriving(Eq, Show)
 
 complex :: (a, a) -> Complex a
 complex = uncurry Complex 
 
 -- unary operators -----------------------------------------
 conjugate :: Num a => Complex a -> Complex a
-conjugate (Complex real imag) = 
-    Complex real ( -imag ) 
+conjugate (Complex real imag) = Complex real ( -imag ) 
 
 abs :: Floating a => Complex a -> a
-abs (Complex real imag) = 
-    sqrt ( real ^ 2 + imag ^ 2 ) 
+abs (Complex re im) = sqrt ( re ^ 2 + im ^ 2 ) 
 
 real :: Num a => Complex a -> a
-real (Complex real _) = real 
+real (Complex re _) = re 
 
 imaginary :: Num a => Complex a -> a
 imaginary (Complex _ imag) = imag 
 
 exp :: Floating a => Complex a -> Complex a
-exp (Complex real imag) = 
-    Complex (Std.exp real * Std.cos imag) 
-            (Std.exp real * Std.sin imag)
+exp (Complex re im) = 
+    Complex (Std.exp re * Std.cos im) (Std.exp re * Std.sin im)
 
 -- binary operators ----------------------------------------
 mul :: Num a => Complex a -> Complex a -> Complex a
-mul (Complex re1 im1) (Complex re2 im2) = 
-    Complex (re1 * re2 - im1 * im2) (re1 * im2 + re2 * im1)
+mul (Complex re1 im1) (Complex re2 im2) = Complex (re1 * re2 - im1 * im2) 
+                                                  (re1 * im2 + re2 * im1)
 
 add :: Num a => Complex a -> Complex a -> Complex a
-add (Complex re1 im1) (Complex re2 im2) = 
-    Complex (re1 + re2) (im1 + im2)
+add (Complex re1 im1) (Complex re2 im2) = Complex (re1 + re2) (im1 + im2)
 
 sub :: Num a => Complex a -> Complex a -> Complex a
-sub (Complex re1 im1) (Complex re2 im2) = 
-    Complex (re1 - re2) (im1 - im2)
+sub (Complex re1 im1) (Complex re2 im2) = Complex (re1 - re2) (im1 - im2)
 
 div :: Fractional a => Complex a -> Complex a -> Complex a
-div (Complex re1 im1) (Complex re2 im2) = 
-    Complex ((re1 * re2 + im1 * im2) / d) 
-            ((im1 * re2 - im2 * re1) / d)
-    where 
+div (Complex re1 im1) (Complex re2 im2) =
+    let 
         d = re2 ^^ 2 + im2 ^^ 2
+    in
+        Complex ((re1 * re2 + im1 * im2) / d) ((im1 * re2 - im2 * re1) / d)
