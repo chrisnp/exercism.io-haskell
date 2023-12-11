67f7715ee3d1f1aea524becee11b4c28250d6427
Invigo <invigo.devel@gmail.com>
2023-12-11 23:49:28 +0200

    Update ResistorColors.hs

--- resistor-color-duo/src/ResistorColors.hs
+++ resistor-color-duo/src/ResistorColors.hs
@@ -11,21 +11,9 @@ data Color =
   | Violet
   | Grey
   | White
-  deriving (Eq, Show, Read)
+  deriving (Eq, Show, Enum, Bounded)
 
-colorValue :: Color -> Int
-colorValue color = 
-  case color of
-    Black  -> 0
-    Brown  -> 1
-    Red    -> 2
-    Orange -> 3
-    Yellow -> 4
-    Green  -> 5
-    Blue   -> 6
-    Violet -> 7
-    Grey   -> 8
-    White  -> 9
+value :: (Color, Color) -> Int
+value = uncurry ((. fromEnum) . (+) . (10 *) . fromEnum)
+  -- foldl ((+) . (*10)) 0 (colorValue <$> (a, b))
 
-value :: [Color] -> Int
-value cs = foldl ((+) . (*10)) 0 (colorValue <$> cs)
