e4f6a1d028592144ee7d94b4d23df117a85e19cf
Invigo <invigo.devel@gmail.com>
2024-01-09 20:37:24 +0200

    Update Diamond.hs

--- diamond/src/Diamond.hs
+++ diamond/src/Diamond.hs
@@ -5,21 +5,13 @@ import Data.Char
 diamond :: Char -> Maybe [String]
 diamond 'A' = Just ["A"] 
 diamond  ch 
-    | not (ch `elem` ['A'..'Z']) = 
-        Nothing
-    | otherwise = 
-        Just [draw n i | i <- [0 .. n] ++ 
-                              [n - 1, n - 2 .. 0]]
+    | not (ch `elem` ['A'..'Z']) = Nothing
+    | otherwise = Just [draw n i | i <- [0 .. n] ++ [n - 1, n - 2 .. 0]]
     where 
-        n = 
-            ord ch - ord 'A'
-        draw x 0 = 
-            (replicate x ' ') ++ 
-            ['A'] ++ 
-            (replicate x ' ')
-        draw x j = 
-            (replicate (x - j) ' ') ++ 
-            [chr (j + ord 'A')] ++ 
-            (replicate (j * 2 - 1) ' ') ++ 
-            [chr (j + ord 'A')] ++ 
-            (replicate (x - j) ' ')
+        n = ord ch - ord 'A'
+        draw x 0 = (replicate x ' ') ++ ['A'] ++ (replicate x ' ')
+        draw x j = (replicate (x - j) ' ') ++ 
+                   [chr (j + ord 'A')] ++ 
+                   (replicate (j * 2 - 1) ' ') ++ 
+                   [chr (j + ord 'A')] ++ 
+                   (replicate (x - j) ' ')
