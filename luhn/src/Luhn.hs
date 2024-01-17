6b40c3e07da58183db31dcdfbe9656176001c0ed
Invigo <invigo.devel@gmail.com>
2024-01-17 21:37:49 +0200

    Refactor Luhn.hs

--- luhn/src/Luhn.hs
+++ luhn/src/Luhn.hs
@@ -4,12 +4,13 @@ import Data.Char (digitToInt)
 
 isValid :: String -> Bool
 isValid n = 
-    case filter (/= ' ') n of
-        ""  -> False
-        [_] -> False
-        x   -> checksum x == 0
-    where
-    checksum = (`mod` 10) . luhn . reverse . map digitToInt
-    luhn []  = 0
-    luhn [x] = x
-    luhn (x0:x1:xs) = x0 + 2 * x1 - (if x1 >= 5 then 9 else 0) + (luhn xs)
+    let
+        checksum = (`mod` 10) . luhn . reverse . map digitToInt
+        luhn []  = 0
+        luhn [x] = x
+        luhn (x0:x1:xs) = x0 + 2 * x1 - (if x1 >= 5 then 9 else 0) + (luhn xs)
+    in
+        case filter (/= ' ') n of
+            ""  -> False
+            [_] -> False
+            x   -> checksum x == 0
