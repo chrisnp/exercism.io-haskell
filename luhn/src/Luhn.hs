0a9cb9382909b1525e1cbce8d57f9d128255bced
Invigo <invigo.devel@gmail.com>
2024-01-17 21:32:51 +0200

    Update Luhn.hs

--- luhn/src/Luhn.hs
+++ luhn/src/Luhn.hs
@@ -3,12 +3,13 @@ module Luhn (isValid) where
 import Data.Char (digitToInt)
 
 isValid :: String -> Bool
-isValid n = case filter (/= ' ') n of
-    ""  -> False
-    [_] -> False
-    x   -> checksum x == 0
+isValid n = 
+    case filter (/= ' ') n of
+        ""  -> False
+        [_] -> False
+        x   -> checksum x == 0
     where
-        checksum = (`mod` 10) . luhn . reverse . map digitToInt
-        luhn []  = 0
-        luhn [x] = x
-        luhn (x0:x1:xs) = x0 + 2 * x1 - (if x1 >= 5 then 9 else 0) + (luhn xs)
+    checksum = (`mod` 10) . luhn . reverse . map digitToInt
+    luhn []  = 0
+    luhn [x] = x
+    luhn (x0:x1:xs) = x0 + 2 * x1 - (if x1 >= 5 then 9 else 0) + (luhn xs)
