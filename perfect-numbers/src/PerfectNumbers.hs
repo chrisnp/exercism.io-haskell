a23928f8d384ef89d8d21bb6c662fea68e8b3fad
Invigo <invigo.devel@gmail.com>
2024-01-19 02:02:07 +0200

    Refactor PerfectNumbers

--- perfect-numbers/src/PerfectNumbers.hs
+++ perfect-numbers/src/PerfectNumbers.hs
@@ -2,34 +2,32 @@ module PerfectNumbers (classify, Classification(..)) where
 
 import Data.List (nub)
 
-data Classification = Deficient | 
-                      Perfect | 
-                      Abundant 
-                      deriving (Eq, Show)
+data Classification = Deficient 
+                    | Perfect 
+                    | Abundant 
+                    deriving (Eq, Show)
 
 classify :: Int -> Maybe Classification
-classify n 
-    | n <= 0        = Nothing
-    | aliquot == n  = Just Perfect
-    | aliquot > n   = Just Abundant
-    | otherwise     = Just Deficient
-        where 
-            aliquot = aliquotSum n
+classify n =
+    let 
+        aliquot = aliquotSum n
+    in 
+        if n <= 0 then Nothing
+        else if aliquot < n then Just Deficient
+        else if aliquot > n then Just Abundant
+        else Just Perfect
 
 factors :: Int -> [Int]
 factors n = 
     let
-        semiFactors x = 
-            takeWhile (\y -> y * y <= x) 
-                      [z | z <- [2..x-1], 
-                                x `mod` z == 0]
+        semiFactors x = takeWhile (\y -> y * y <= x) 
+                        [z | z <- [2..x-1], x `mod` z == 0]
         xs = semiFactors n
     in
-        nub (1 : (xs ++ (map (n `div`) xs)))
-        
-                
+        nub (1 : (xs ++ (map (n `div`) xs)))  
 
 aliquotSum :: Int -> Int
-aliquotSum n 
-    | n == 1    = 0
-    | otherwise = foldl (+) 0 (factors n)
+aliquotSum n =
+    case n of
+        1 -> 0
+        _ -> foldl (+) 0 (factors n)
