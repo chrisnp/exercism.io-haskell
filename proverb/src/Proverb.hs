187145c5dc84885d1d8a59ef6ad3da80b1dffafc
Invigo <invigo.devel@gmail.com>
2024-02-07 13:23:32 +0200

    Refactor Proverb

--- proverb/src/Proverb.hs
+++ proverb/src/Proverb.hs
@@ -2,24 +2,23 @@ module Proverb(recite) where
 
 import Text.Printf
 
-pairs :: [a] -> [(a, a)]
-pairs xs = case xs of 
-    [] -> []
-    ys -> zipWith (,) ys (tail ys)
-
 recite :: [String] -> String
 recite xs = 
     let 
         premise :: (String, String) -> String
-        premise = uncurry (printf "For want of a %s the %s was lost.\n")
+        premise = uncurry $ printf "For want of a %s the %s was lost.\n"
         conclusion :: String -> String
         conclusion = printf "And all for the want of a %s."
     in
         case xs of 
             [] -> []
-            ys -> 
-                let
+            ys -> let
                     verses =  map premise (pairs ys)
                     epilogue = conclusion $ head ys
-                in 
+                  in 
                     concat $ (++) verses [epilogue]
+
+-- Auxiliary
+
+pairs :: [a] -> [(a, a)]
+pairs = zip <*> tail
