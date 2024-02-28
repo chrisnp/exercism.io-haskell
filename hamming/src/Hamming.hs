d4fe3f08774f3d244309f22a802225a625f1df1e
Invigo <invigo.devel@gmail.com>
2024-02-28 22:10:30 +0200

    Update Hamming.hs

--- hamming/src/Hamming.hs
+++ hamming/src/Hamming.hs
@@ -2,6 +2,6 @@ module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
 distance xs ys 
-    |length xs == length ys = 
+    | length xs == length ys = 
             (Just . length . filter not) $ zipWith (==) xs ys
-    |otherwise = Nothing
+    | otherwise = Nothing
