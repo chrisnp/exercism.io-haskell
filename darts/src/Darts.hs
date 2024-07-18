f92cbf7fc672d8577100765042b55e2415e2ac04
Invigo <invigo.devel@gmail.com>
2024-07-18 22:01:15 +0300

    Update Darts.hs

--- darts/src/Darts.hs
+++ darts/src/Darts.hs
@@ -1,4 +1,10 @@
 module Darts (score) where
 
 score :: Float -> Float -> Int
-score x y = error "You need to implement this function."
+score x y
+    | radius > 10 = 0
+    | radius >  5 = 1
+    | radius >  1 = 1 + 4
+    | otherwise   = 1 + 4 + 5
+    where 
+        radius = (x ** 2 + y ** 2) ** 0.5
