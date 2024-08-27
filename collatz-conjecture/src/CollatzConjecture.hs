050b55ac8b12f08f8e755e5501186bc7e1d6b1bb
Invigo <invigo.devel@gmail.com>
2024-08-28 01:01:04 +0300

    Reformat CollatzConjecture.hs

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -4,12 +4,16 @@ import Data.List
 import Control.Monad ( liftM2 )
 
 collatzStep :: Integer -> Integer
-collatzStep = (liftM2 if' even (`quot` 2)) <*> ((1 +) . (3 *))
+collatzStep = 
+    (liftM2 if' even (`quot` 2)) 
+    <*> 
+    ((1 +) . (3 *))
 
 collatz :: Integer -> Maybe Integer
-collatz = flip (liftM2 if' (> 0) 
-                    ((toInteger <$>) . elemIndex 1 . iterate collatzStep))
-                    Nothing
+collatz = 
+    flip 
+    (liftM2 if' (> 0) ((toInteger <$>) . elemIndex 1 . iterate collatzStep))
+    Nothing
 
 -- Auxiliary
 
