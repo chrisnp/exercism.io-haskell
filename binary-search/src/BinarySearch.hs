3eec0c712afb33e6c48017969ef3845e0a60a391
Invigo <invigo.devel@gmail.com>
2024-08-27 23:22:57 +0300

    Update BinarySearch.hs

--- binary-search/src/BinarySearch.hs
+++ binary-search/src/BinarySearch.hs
@@ -11,10 +11,11 @@ find = search <*> bounds
 -- Auxiliary --
 
 search :: Ord a => Array Int a -> (Int, Int) -> a -> Maybe Int
-search array (min, max) x 
-    | min > max     = Nothing
-    | x < array!mid = search array (min, mid - 1) x
-    | x > array!mid = search array (mid + 1, max) x
-    | otherwise     = Just mid
-    where 
-        mid = shiftR (min + max) 1 
+search array (min, max) x
+ 
+    -- | min > max     = Nothing
+    -- | x < array!mid = search array (min, mid - 1) x
+    -- | x > array!mid = search array (mid + 1, max) x
+    -- | otherwise     = Just mid
+    -- where 
+    --     mid = shiftR (min + max) 1 
