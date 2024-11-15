9eb36393bc0a42f78e5de564af4b108ab2a26499
Invigo <invigo.devel@gmail.com>
2024-11-15 22:31:18 +0200

    Update BankAccount.hs

--- bank-account/src/BankAccount.hs
+++ bank-account/src/BankAccount.hs
@@ -13,24 +13,20 @@ data BankAccount = BankAccount { _balance :: MVar (Maybe Integer) }
 
 closeAccount :: BankAccount -> IO ()
 closeAccount account = do
-    let 
-        balance = _balance account
+    let balance = _balance account
     _ <- swapMVar balance Nothing
     return ()
 
 getBalance :: BankAccount -> IO (Maybe Integer)
 getBalance account = do
-    let 
-        balance = _balance account
+    let balance = _balance account
     readMVar balance
 
 incrementBalance :: BankAccount -> Integer -> IO (Maybe Integer)
 incrementBalance account amount = do
-    let 
-        balance = _balance account
+    let balance = _balance account
     oldBalance <- takeMVar balance
-    let 
-        newBalance = (+ amount) <$> oldBalance
+    let newBalance = (+ amount) <$> oldBalance
     putMVar  balance $!! newBalance
     return newBalance
 
