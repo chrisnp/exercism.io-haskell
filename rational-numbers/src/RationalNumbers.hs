56da7f5cbddd1731cd56e9bf46ebb557553518f8
Invigo <invigo.devel@gmail.com>
2025-01-14 19:17:08 +0200

    RationalNumbers.hs passing all

--- rational-numbers/src/RationalNumbers.hs
+++ rational-numbers/src/RationalNumbers.hs
@@ -15,39 +15,92 @@ module RationalNumbers
 import Prelude hiding (div, abs, Rational)
 
 -- Data definition -------------------------------------------------------------
-data Rational a = Dummy deriving(Eq, Show)
+data Rational a = Rational {num, den :: a} deriving(Eq, Show)
 
 rational :: Integral a => (a, a) -> Rational a
-rational = error "You need to implement this function"
+rational (n, d) = 
+    let 
+        hcf = gcd n d
+        nr = n `quot` hcf
+        dr = d `quot` hcf
+        sign = if dr < 0 then -1 else 1 
+    in  
+        Rational { num = sign * nr, den = sign * dr }
 
 -- unary operators -------------------------------------------------------------
 abs :: Integral a => Rational a -> Rational a
-abs = error "You need to implement this function"
+abs r = 
+    let 
+        n = numerator r 
+        d = denominator r 
+    in 
+        rational (n * (signum n), d * (signum d))
 
 numerator :: Integral a => Rational a -> a
-numerator = error "You need to implement this function"
+numerator r = let Rational n _ = r in n
 
 denominator :: Integral a => Rational a -> a
-denominator = error "You need to implement this function"
+denominator r = let Rational _ d = r in d
 
 -- binary operators ------------------------------------------------------------
 add :: Integral a => Rational a -> Rational a -> Rational a
-add = error "You need to implement this function"
+add r1 r2 =
+    let 
+        n1 = numerator r1
+        d1 = denominator r1
+        n2 = numerator r2
+        d2 = denominator r2
+    in 
+        rational (n1 * d2 + n2 * d1, d1 * d2)
 
 sub :: Integral a => Rational a -> Rational a -> Rational a
-sub = error "You need to implement this function"
+sub r1 r2 =
+    let
+        n2 = numerator r2
+        d2 = denominator r2
+    in
+        add r1 (rational ((-n2), d2))
 
 mul :: Integral a => Rational a -> Rational a -> Rational a
-mul = error "You need to implement this function"
+mul r1 r2 = 
+    let 
+        n1 = numerator r1
+        d1 = denominator r1
+        n2 = numerator r2
+        d2 = denominator r2
+    in  
+        rational (n1 * n2, d1 * d2)
 
 div :: Integral a => Rational a -> Rational a -> Rational a
-div = error "You need to implement this function"
+div r1 r2 =
+    let 
+        n2 = numerator r2
+        d2 = denominator r2
+    in  
+        mul r1 (rational (d2, n2))
 
 pow :: Integral a => Rational a -> a -> Rational a
-pow = error "You need to implement this function"
+pow r e = 
+    let 
+        n = numerator r
+        d = denominator r
+    in 
+        if e >= 0
+        then rational (n ^ e, d ^ e) 
+        else rational (d ^ (-e), n ^ (-e))
 
 expRational :: Integral a => Floating b => Rational a -> b -> b
-expRational = error "You need to implement this function"
+expRational r e = 
+    let 
+        n = numerator r
+        d = denominator r
+    in
+        (fromIntegral n ** e) / (fromIntegral d ** e)
 
 expReal :: Floating a => Integral b => a -> Rational b -> a
-expReal = error "You need to implement this function"
+expReal e r = 
+    let 
+        n = numerator r
+        d = denominator r
+    in
+        e ** (fromIntegral n / fromIntegral d)
